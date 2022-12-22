46c472e36821248bba2bab2e23466eaf59070fe5
Arkadashim <andrewsha.antonov@mail.ru>
2022-12-22 17:35:38 +0300

    update

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
 module.exports = new Sequelize({
     database: 'codegig_studproject',
     username: 'postgres',
     password: 'rootroot',
     host: 'localhost',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectModule: pg
 });
